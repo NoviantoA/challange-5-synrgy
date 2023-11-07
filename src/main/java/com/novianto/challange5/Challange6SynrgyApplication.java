b43beddbb99b85e52f1f94e4ce56b25576795ca5
M. Novianto Anggoro <20082010132@student.upnjatim.ac.id>
2023-11-07 23:25:13 +0700

    add swagger documentation api

--- src/main/java/com/novianto/challange5/Challange6SynrgyApplication.java
+++ src/main/java/com/novianto/challange5/Challange6SynrgyApplication.java
@@ -1,9 +1,11 @@
 package com.novianto.challange5;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class Challange6SynrgyApplication {
 
 	public static void main(String[] args) {
